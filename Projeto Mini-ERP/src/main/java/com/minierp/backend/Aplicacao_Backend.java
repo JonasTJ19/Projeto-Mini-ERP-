fd13bac3ee9b92f6148c32402ea52397ca589d02
Jonas Tillmann Júnior <151531222+JonasTJ19@users.noreply.github.com>
2025-11-18 23:19:38 -0300

    Primeiro Commit Do projeto

---
+++ Projeto Mini-ERP/src/main/java/com/minierp/backend/Aplicacao_Backend.java
@@ -0,0 +1,14 @@
+package com.minierp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Aplicacao_Backend {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(Aplicacao_Backend.class, args);
+	}
+
+}
